b66a78b52765212cbe8aadf39a5d51126ec17984
Shiftgithub <60180521+Shiftgithub@users.noreply.github.com>
2020-08-27 21:27:45 +0600

    Joy -> display something

--- Assingments/Bishawjit/File Handling/file.php
+++ Assingments/Bishawjit/File Handling/file.php
@@ -1,6 +1,14 @@
 <?php
  $myfile = fopen("testfile.txt", "w");
- $txt = "I am Joy. I am doing file handling for the first time.";
- fwrite($myfile, $txt);
+ $txt = "I am Joy. I am doing file handling for the first time. 
+        Why Bro ???";
+
+    if(fwrite($myfile, $txt)){
+        echo "File Created";
+    }
+    else {
+        echo "File not Created";
+    }
+ 
  fclose($myfile);
 ?>
