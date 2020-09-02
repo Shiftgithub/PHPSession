7baf7c6910ef5dda17b12abdfccd50d04a0f0b35
Shiftgithub <60180521+Shiftgithub@users.noreply.github.com>
2020-09-02 20:55:20 +0600

    Formate

--- Assingments/Rupa/File-handling/formresult.php
+++ Assingments/Rupa/File-handling/formresult.php
@@ -1,53 +1,52 @@
 <html>
+
 <head>
 	<style type="text/css">
-		table
-		{
-		 background: linear-gradient(to bottom, #0000ff 0%, #66ccff 100%);
-		color: white;
+		table {
+			background: linear-gradient(to bottom, #0000ff 0%, #66ccff 100%);
+			color: white;
 		}
 	</style>
 </head>
 <boby>
 
-<?php
-$btn=$_POST["btn"];
-$search=$_POST["search"];
-if (isset($_POST[$btn])) {
+	<?php
+	$btn = $_POST["btn"];
 
-	fopen("$username.txt",a);
-}
+	$search = $_POST["search"];
+	if (isset($_POST[$btn])) {
 
-if($_SERVER["REQUEST_METHOD"] == "POST")
-
-{
-	$username=$_POST["username"];
-	$email=$_POST["email"];
-	$password=$_POST["password"];
-if ($search==$username) {
-	echo"Name :".$username. "<br> 
-	 Email :".$email."<br>
-	 password :".$password."<br>";
-}
+		fopen("$username.txt", a);
 	}
-	
-	echo"Name :".$username. "<br> 
-	 Email :".$email."<br>
-	 password :".$password."<br><a href='search in form.php'><input type='submit'value='Home' name='back'></a> ";
 
-	 extract($_REQUEST);
-if($file=fopen("$username.txt","a"));
-{
-	
-	
-fwrite($file, "Name :");
-fwrite($file, $username ."\n");
-fwrite($file, "Email :");
-fwrite($file, $email ."\n");
-fwrite($file, "Password :");
-fwrite($file, $password ."\n");
-fclose($file);
-}
-?>
-</body>
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		$username = $_POST["username"];
+		$email = $_POST["email"];
+		$password = $_POST["password"];
+		if ($search == $username) {
+			echo "Name :" . $username . "<br> 
+			Email :" . $email . "<br>
+			password :" . $password . "<br>";
+		}
+	}
+
+	echo "Name :" . $username . "<br> 
+		Email :" . $email . "<br>
+		password :" . $password . "<br><a href='search in form.php'><input type='submit'value='Home' name='back'></a> ";
+
+	extract($_REQUEST);
+	if ($file = fopen("$username.txt", "a")); {
+
+
+		fwrite($file, "Name :");
+		fwrite($file, $username . "\n");
+		fwrite($file, "Email :");
+		fwrite($file, $email . "\n");
+		fwrite($file, "Password :");
+		fwrite($file, $password . "\n");
+		fclose($file);
+	}
+	?>
+	</body>
+
 </html>
