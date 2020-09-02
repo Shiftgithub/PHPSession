84c732854255f120bf903af617c8d6c2a2dcb883
Shiftgithub <60180521+Shiftgithub@users.noreply.github.com>
2020-09-02 21:12:59 +0600

    FileRead

--- Assingments/Rupa/File-handling/fileOpen.php
+++ Assingments/Rupa/File-handling/fileOpen.php
@@ -1,8 +1,8 @@
 <?php
     $username = "1234";
-    $file = fopen("$username.txt", "r");
-    echo "Hello";
-    fread($file, filesize("$username.txt"));
+    $file = fopen("$username.txt", "r") or die("Unable to open file!");
+    
+    echo fread($file, filesize("$username.txt"));
 
     fclose($file);
 ?>
