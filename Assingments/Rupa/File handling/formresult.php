e3c41493ede2b63b82229f1809c0c6c2f0f219a3
Jannat267 <jannatulferdouserupali@gmail.com>
2020-09-02 22:34:29 +0600

    new

--- Assingments/Rupa/File handling/formresult.php
+++ Assingments/Rupa/File handling/formresult.php
@@ -11,25 +11,23 @@
 <boby>
 
 <?php
-$btn=$_POST["btn"];
-$search=$_POST["search"];
-if (isset($_POST[$btn])) {
 
-	fopen("$username.txt",a);
+$search=$_POST["search"];
+if (isset($_POST["btn"])) {
+
+$s=fopen("$search.txt", "r") or die("File doesn't exist");
+	
+fclose($s);
 }
 
-if($_SERVER["REQUEST_METHOD"] == "POST")
+if(isset($_POST["save"]))
 
 {
 	$username=$_POST["username"];
 	$email=$_POST["email"];
 	$password=$_POST["password"];
-if ($search==$username) {
-	echo"Name :".$username. "<br> 
-	 Email :".$email."<br>
-	 password :".$password."<br>";
-}
-	}
+
+	
 	
 	echo"Name :".$username. "<br> 
 	 Email :".$email."<br>
@@ -39,7 +37,6 @@ if ($search==$username) {
 if($file=fopen("$username.txt","a"));
 {
 	
-	
 fwrite($file, "Name :");
 fwrite($file, $username ."\n");
 fwrite($file, "Email :");
@@ -47,6 +44,7 @@ fwrite($file, $email ."\n");
 fwrite($file, "Password :");
 fwrite($file, $password ."\n");
 fclose($file);
+}
 }
 ?>
 </body>
