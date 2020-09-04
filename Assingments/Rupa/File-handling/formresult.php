ff946a2b58a12ac33cf2810db590af94cd2de9b2
Jannat267 <jannatulferdouserupali@gmail.com>
2020-09-04 19:45:36 +0600

    updates

--- Assingments/Rupa/File-handling/formresult.php
+++ Assingments/Rupa/File-handling/formresult.php
@@ -3,41 +3,57 @@
 <head>
 	<style type="text/css">
 		table {
-			background: linear-gradient(to bottom, #0000ff 0%, #66ccff 100%);
-			color: white;
+			margin:30px;
+			width: 40%;
+			background: linear-gradient(to bottom, #99ccff 0%, #ccffff 100%);
+			color:black;
+			font-size: 25px;
 		}
+
 	</style>
 </head>
 <boby>
+
 
 	<?php
 
 	if (isset($_POST["btn"])) {
 		$search = $_POST["search"];
 		$s = fopen("$search.txt", "r") or die("File doesn't exist");
+		echo fread($s, filesize("$search.txt"));
 		fclose($s);
 	}
 
 	if (isset($_POST["save"])) {
 		$username = $_POST["username"];
-		$email = $_POST["email"];
-		$password = $_POST["password"];
+		$name=$_POST["name"];
+		$number=$_POST["number"];
+        $email = $_POST["email"];
+		$address = $_POST["address"];
 
 
 
-		echo "Name :" . $username . "<br> 
-			Email :" . $email . "<br>
-			password :" . $password . "<br><a href='search in form.php'><input type='submit'value='Home' name='back'></a> ";
+		echo "<table border=1><tr><td>
+		      Student's Name :</td><td>" . $name . "</td></tr> 
+		      <tr><td>ID :</td><td>" . $username . "</td> </tr>
+		      <tr><td>Email :</td><td>" . $email . "</td> </tr>
+			<tr><td>Contact Number :</td><td>" . $number . "</td> </tr>
+			<tr><td>Address :</td><td>" . $address . "</td> </tr></table>";
+			echo "<br><a href='search in form.php'><input type='submit'value='Home' name='back'></a> ";
 
 		extract($_REQUEST);
 		if ($file = fopen("$username.txt", "a")); {
 
+			fwrite($file, "Student's ID :");echo "</td><td>";
+			fwrite($file, $username . "\n");echo "</td></tr>";
 			fwrite($file, "Name :");
-			fwrite($file, $username . "\n");
+			fwrite($file, $name . "\n");
 			fwrite($file, "Email :");
 			fwrite($file, $email . "\n");
-			fwrite($file, "Password :");
-			fwrite($file, $password . "\n");
+			fwrite($file, "Contact number :");
+			fwrite($file, $number."\n");
+			fwrite($file, "Address :");
+			fwrite($file,$address."\n");
 			fclose($file);
 		}
 	}
--- Assingments/Rupa/File-handling/searchInForm.php
+++ Assingments/Rupa/File-handling/searchInForm.php
@@ -1,21 +1,55 @@
 <html>
 
 <head>
-	<titel>Application Form</title>
+	<titel>Information Form</title>
 		<style>
+		body{
+			background:linear-gradient(to bottom, #99ccff 0%, #ccffff 100%);
+		}
 			.box {
-				height: 230px;
-				width: 400px;
+				height: 350px;
+				width: 370px;
 				background: #383838;
 				margin-left: 200px;
-				margin-top: 200px;
+				margin-top: 100px;
 				box-shadow: 1px 1px 2px black, 0 0 25px #696969, 0 0 5px darkblue;
 				border-radius: 2%;
 			}
 
 			.input-box {
-				margin: 10px;
+				margin: 20px;
 			}
+			.btn-btn{
+				background-color:  #ff4d4d;
+				border: none;
+                border-radius: 15px;
+                font-size: 16px;
+                width: 70px;
+			}
+			
+			.btn-btn:hover {background:red;}
+			.btn{
+
+				display: inline-block;
+  padding: 5px 10px;
+  font-size: 16px;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+  outline: none;
+  color: #fff;
+  background-color:  #ff4d4d;
+  border: none;
+  border-radius: 10px;
+			}
+
+.btn:hover {background: #ff9999;}
+
+.btn:active {
+  background-color: #3e8e41;
+  box-shadow: 0 5px #666;
+  transform: translateY(4px);
+}
 		</style>
 </head>
 
@@ -28,18 +62,26 @@
 				<button class="btn" name="btn">Search</button>
 			</div>
 			<div class="input-box">
-				<label for="" style=" color: white;">Username</label>
-				<input type="text" name="username" autocomplete="off">
+				<label for="" style=" color: white;">Student's ID:</label>
+				<input type="text" name="username" autocomplete="off" style="margin-left: 52px;">
 			</div>
 			<div class="input-box">
-				<label style="margin-left: 28px;  color: white;">Email</label>
-				<input type="email" name="email" autocomplete="off">
+				<label style="margin-left: 5px;  color: white;">Name:</label>
+				<input type="text" name="name" autocomplete="off" style="margin-left: 89px;">
 			</div>
 			<div class="input-box">
-				<label style="margin-left: 5px; color: white;">Password </label>
-				<input type="password" name="password" autocomplete="off">
+				<label style="margin-left: 5px;  color: white;">Email:</label>
+				<input type="email" name="email" autocomplete="off" style="margin-left: 89px;">
 			</div>
-			<input type="submit" name="save" value="Save"/ style="color:white; background:#A9A9A9;margin: 10px;padding: 8px;border-radius: 10%; ">
+			<div class="input-box">
+				<label style="margin-left: 5px;  color: white;">Contact Number:</label>
+				<input type="number" name="number" autocomplete="off"style="margin-left: 22px;">
+			</div>
+			<div class="input-box">
+				<label style="margin-left: 5px; color: white;">Address: </label>
+				<input type="text" name="address" autocomplete="off"style="margin-left: 75px;">
+			</div>
+			<input type="submit" class="btn-btn" name="save" value="Save"/ style="color:white; background:#380000;margin-left:22px;padding: 8px;border-radius: 10%; ">
 		</form>
 	</div>
 </body>
