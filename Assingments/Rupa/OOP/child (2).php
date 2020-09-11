9d6ca84b7c2bf42a761ac5c0165900888efb6a9d
Shiftgithub <60180521+Shiftgithub@users.noreply.github.com>
2020-09-11 12:09:09 +0600

    Space Removed

--- Assingments/Rupa/OOP/child (2).php
+++ Assingments/Rupa/OOP/child (2).php
@@ -1,4 +1,3 @@
-
 <!doctype html>
 <html lang="en">
 
@@ -11,94 +10,72 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
+
 <body>
-	<form method="POST">
-	<input type="number" name="num1" >
-   <input type="number" name="num2">
-   <input type="submit" name="add" value="add">
-   <input type="submit" name="sub" value="sub">
-   <input type="submit" name="mul" value="mul">
-   <input type="submit" name="div" value="div"></form>
-<?php
+    <form method="POST">
+        <input type="number" name="num1">
+        <input type="number" name="num2">
+        <input type="submit" name="add" value="add">
+        <input type="submit" name="sub" value="sub">
+        <input type="submit" name="mul" value="mul">
+        <input type="submit" name="div" value="div"></form>
+    <?php
     /**
      * @class Head
      * 
      */
-    class Head{
+    class Head
+    {
 
-        public function add($num1,$num2)
+        public function add($num1, $num2)
         {
-            echo $num1+$num2."<br> ";
-           
+            echo $num1 + $num2 . "<br> ";
         }
-		public function sub($num1,$num2)
+        public function sub($num1, $num2)
         {
-			
-            echo $num1-$num2."<br> ";
-           
+
+            echo $num1 - $num2 . "<br> ";
         }
-		public function mul($num1,$num2)
+        public function mul($num1, $num2)
         {
-			
-            echo $num1*$num2."<br> ";
-           
+
+            echo $num1 * $num2 . "<br> ";
         }
-		public function div($num1,$num2)
+        public function div($num1, $num2)
         {
-			
-            echo $num1/$num2."<br>";
-           
+
+            echo $num1 / $num2 . "<br>";
         }
     }
 
-    
-    if(isset($_POST["add"]))
-	{
-		$n1=$_POST["num1"];
-        $n2=$_POST["num2"];
-		$ad=new Head();
-		$result = $ad->add($n1,$n2);
-        print_r($result);
-	}
-        
-	elseif(isset($_POST["sub"]))
-	{
-		$n1=$_POST["num1"];
-        $n2=$_POST["num2"];
-        $su=new Head();
-        $result = $su->sub($n1,$n2);
-        print_r($result);
 
-	}
-		
-	elseif(isset($_POST["mul"]))
-
-		{
-			$n1=$_POST["num1"];
-        $n2=$_POST["num2"];
+    if (isset($_POST["add"])) {
+        $n1 = $_POST["num1"];
+        $n2 = $_POST["num2"];
+        $ad = new Head();
+        $result = $ad->add($n1, $n2);
+        print_r($result);
+    } elseif (isset($_POST["sub"])) {
+        $n1 = $_POST["num1"];
+        $n2 = $_POST["num2"];
+        $su = new Head();
+        $result = $su->sub($n1, $n2);
+        print_r($result);
+    } elseif (isset($_POST["mul"])) {
+        $n1 = $_POST["num1"];
+        $n2 = $_POST["num2"];
         $mu = new Head();
-        $result = $mu->mul($n1,$n2);
+        $result = $mu->mul($n1, $n2);
         print_r($result);
-		}
-	elseif(isset($_POST["div"]))
-		{
-			$n1=$_POST["num1"];
-        $n2=$_POST["num2"];
+    } elseif (isset($_POST["div"])) {
+        $n1 = $_POST["num1"];
+        $n2 = $_POST["num2"];
         $di = new Head();
-        $result = $di->div($n1,$n2);
+        $result = $di->div($n1, $n2);
         print_r($result);
-		}
-   ?>
-	
-   
-    
+    }
+    ?>
 
-
-
-    
-   
-    
-    
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
